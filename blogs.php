f0af44cf3a84a3271f4504757ac3347de4285154
Alejandro Jarandilla <alejandroj@publiland.com.ar>
2014-12-02 11:42:50 -0300

    Pagina de Blogs

---
+++ blogs.php
@@ -0,0 +1,281 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+<?php include("inc_head.php"); ?>
+</head>
+
+<body>
+
+<!-- BANNER -->
+<?php include("inc_top-banner.php"); ?>
+<!-- HEADER -->
+<?php include("inc_header.php"); ?>
+<!-- MAIN NAV -->
+<?php include("inc_nav.php"); ?>
+<!-- CATEGORIA -->
+<section class="Row CatHead blog-dashboard">
+	<h2>Blog: <strong>Desde Nueva York</strong> <span>|</span> Tendencias</h2>
+</section>
+<!-- CONTENT -->
+<main class="Row">
+	<div class="Wrapper">
+		
+		<section class="BoxesGrid" id="MsGrid">
+			<div class="grid-sizer"></div>
+			<div class="gutter-sizer"></div>
+				
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/home/pic-03.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Sólo para golosos</a></h3>
+							<p>Tan importante como visitar los museos, recorrer sus barrios y pasear por el Central Park ...</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/blogs/pic-01.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Los subtes de NY tienen ese no sé qué</a></h3>
+							<p>Me animo a decir que todas las personas que visitaron Nueva York en algún momento se ...</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGreen">
+				<div class="Head Single BlogAutor">
+					<a href="#"><img src="images/blogs/autor.jpg" width="45" height="45" alt=""/></a>
+					<h4><strong>Sofia Nougués</strong><br> 
+					Mini-Bío</h4>
+				</div>
+				<div class="InnerBox">
+					<div class="Data">
+						<div class="InnerData">
+							<p>Es Historiadora del Arte y trabaja para ONGs dedicadas a la promoción cultural. En su tiempo libre disfruta de recorrer las ciudades a pie con cámara de fotos en mano. Espera descubrir todos los secretos de esta gran ciudad... y contártelos!</p>
+						</div>
+						<div class="Contacto">
+							<p><a href="#"><i class="fa fa-twitter"></i> <strong>@PopiNougues</strong></a><br>
+							<a href="#"><i class="fa fa-envelope"></i> <strong>sofia.nougues@sophiaonline.com.ar</strong></a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BlogsBox">
+				<h3><strong>Blogs</strong> <span class="IconSep"></span> Sophia</h3>
+				<ul>
+					<li>
+						<figure><a href="#"><img src="images/blogs/thumb-1.jpg" width="155" height="90" alt=""/></a></figure>
+						<div class="Data">
+							<h4><a href="#"><strong>DESDE BUENOS AIRES</strong></a></h4>
+							<p>
+								<span>04.07.2014</span>
+								<a href="#"><strong>Buenos Aires desde abajo</strong></a>
+							</p>
+						</div>
+					</li>
+					<li>	
+						<figure><a href="#"><img src="images/blogs/thumb-2.jpg" width="155" height="90" alt=""/></a></figure>
+						<div class="Data">
+							<h4><a href="#"><strong>DESDE NUEVA YORK</strong></a></h4>
+							<p>
+								<span>04.07.2014</span>
+								<a href="#"><strong>Solo para golosos</strong></a>
+							</p>
+						</div>
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/blogs/thumb-3.jpg" width="155" height="90" alt=""/></a></figure>
+						<div class="Data">
+							<h4><a href="#"><strong>DESDE LONDRES</strong></a></h4>
+							<p>
+								<span>04.07.2014</span></
+								<a href="#"><strong>Paseando por Portobello Road</strong></p>
+							</p>
+						</div>
+					</li>
+					<li>	
+						<figure><a href="#"><img src="images/blogs/thumb-4.jpg" width="155" height="90" alt=""/></a></figure>
+						<div class="Data">
+							<h4><a href="#"><strong>DESDE ESTOCOLMO</strong></a></h4>
+							<p>
+								<span>04.07.2014</span>
+								<a href="#"><strong>Quién es esa chica</strong></a>
+							</p>
+						</div>
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/blogs/thumb-5.jpg" width="155" height="90" alt=""/></a></figure>
+							<div class="Data">
+							<h4><a href="#"><strong>DESDE MIAMI</strong></a></h4>
+							<p>
+								<span>04.07.2014</span>
+								<a href="#"><strong>Darle una chance a la paz</strong></a>
+							</p>
+						</div>
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/blogs/thumb-6.jpg" width="155" height="90" alt=""/></a></figure>
+						<div class="Data">
+							<h4><a href="#"><strong>DESDE MADRID</strong></a></h4>
+							<p>
+								<span>04.07.2014</span>
+								<a href="#"><strong>Un pueblito moderno entre ovejas</strong></a>
+							</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+			
+			<div class="InfoBox Advertise"><a href="#"><img src="images/banners/ad-300x250-b.png" width="300" height="250" alt=""  class="Desktop" /></a></div>
+			
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/blogs/pic-02.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Los mejores "burgers" de NY</a></h3>
+							<p>Una de las preguntas típicas que me hace la gente que viene a visitar Nueva York es: ¿dónde puedo ...</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>		
+			
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/blogs/pic-03.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Postales verdaderas</a></h3>
+							<p>Seguro están todos al tanto de la tormenta que se vivió en Nueva York y otras ciudades del este...</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/blogs/pic-04.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Neoyorkinos a sus bicis</a></h3>
+							<p>Seguro están todos al tanto de la tormenta que se vivió en Nueva York y otras ciudades del este... </p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox Advertise"><a href="#"><img src="images/banners/ad-300x250.png" width="300" height="250" alt="" class="Desktop" /></a></div>
+			
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/blogs/pic-05.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Una alegría en Nueva York</a></h3>
+							<p>Seguro están todos al tanto de la tormenta que se vivió en Nueva York y otras ciudades del este... </p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/blogs/pic-06.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Banksy, un artista suelto en Nueva York</a></h3>
+							<p>Seguro están todos al tanto de la tormenta que se vivió en Nueva York y otras ciudades del este... </p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+			
+			<div class="InfoBox BoxGris">
+				<div class="Head Doble">
+					<h5>04.07.2014</h5>
+				</div>
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/blogs/pic-07.jpg" width="326" height="187" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Verano en Nueva York</a></h3>
+							<p>Tan importante como visitar los museos, recorrer sus barrios y pasear por el Central Park ...</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+		<div class="LoadBar">
+			<a href="#"><strong>+ Posts de este Blog</strong></a>
+		</div>
+	</div>
+</main>
+<!-- BANNER -->
+<?php include("inc_bottom-banner.php"); ?>
+<?php include("inc_footer.php"); ?>
+<?php include("inc_foot.php"); ?>
+</body>
+</html>
