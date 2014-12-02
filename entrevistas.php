c19a85d507888ec402a980025f2e278c4273011e
Alejandro Jarandilla <alejandroj@publiland.com.ar>
2014-12-01 23:47:31 -0300

    Pagina de Videos y Pagina de Entrevistas

---
+++ entrevistas.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+<?php include("inc_head.php"); ?>
+</head>
+
+<body>
+
+<!-- BANNER -->
+<?php include("inc_top-banner.php"); ?>
+<!-- HEADER -->
+<?php include("inc_header.php"); ?>
+<!-- MAIN NAV -->
+<?php include("inc_nav.php"); ?>
+<!-- CATEGORIA -->
+<section class="Row CatHead entrevistas">
+	<h2>Entrevistas</h2>
+</section>
+<!-- CONTENT -->
+<main class="Row">
+	<div class="Wrapper">
+		
+		<section class="BoxesGrid" id="MsGrid">
+			<div class="grid-sizer"></div>
+			<div class="gutter-sizer"></div>
+				
+			<div class="InfoBox BoxGris FullImg">
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/entrevistas/pic-01.jpg" width="326" height="385" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Muriel Santana</a></h3>
+							<p>El humor puede salvarnos</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris FullImg">
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/entrevistas/pic-02.jpg" width="326" height="385" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Selva Almada</a></h3>
+							<p>Hablar de lo que no se habla</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris FullImg">
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/entrevistas/pic-03.jpg" width="326" height="385" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Beatriz Sarlo</a></h3>
+							<p>Por el camino de las ideas</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris FullImg">
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/home/pic-05.jpg" width="326" height="385" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Carolina Aguirre</a></h3>
+							<p>"Estoy ocupada en capturar el momento"</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris FullImg">
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/entrevistas/pic-04.jpg" width="326" height="385" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Melisa Garat</a></h3>
+							<p>No hay éxito sin sacrificio</p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox BoxGris FullImg">
+				<div class="InnerBox">
+					<figure> <a href="#"><img src="images/entrevistas/pic-05.jpg" width="326" height="385" alt=""/></a>
+					</figure>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Leila Guerrero</a></h3>
+							<p>Escribo para organizar un mundo que ... </p>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+		<div class="LoadBar">
+			<a href="#"><strong>+ Entrevistas</strong></a>
+		</div>
+	</div>
+</main>
+<!-- BANNER -->
+<?php include("inc_bottom-banner.php"); ?>
+<?php include("inc_footer.php"); ?>
+<?php include("inc_foot.php"); ?>
+</body>
+</html>
---
+++ videos.php
@@ -0,0 +1,152 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+<?php include("inc_head.php"); ?>
+</head>
+
+<body>
+
+<!-- BANNER -->
+<?php include("inc_top-banner.php"); ?>
+<!-- HEADER -->
+<?php include("inc_header.php"); ?>
+<!-- MAIN NAV -->
+<?php include("inc_nav.php"); ?>
+<!-- CATEGORIA -->
+<section class="Row CatHead videos">
+	<h2>Videos <span><a href="#">Canal sophia</a><i class="fa fa-youtube"></i></span></h2>
+</section>
+<!-- CONTENT -->
+<main class="Row">
+	<div class="Wrapper">
+		
+		<section class="BoxesGrid" id="MsGrid">
+			<div class="grid-sizer"></div>
+			<div class="gutter-sizer"></div>
+				
+			<div class="InfoBox">
+				<div class="InnerBox">
+					<figure class="Video">
+						<iframe width="560" height="315" src="//www.youtube.com/embed/c9SUAcNlVQ4" frameborder="0" allowfullscreen></iframe>
+					</figure>
+					<div class="Categoria genero">
+						<h4><a href="#"><strong>Género</strong></a></h4>
+					</div>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Emma Watson asombró al mundo</a></h3>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox">
+				<div class="InnerBox">
+					<figure class="Video">
+						<iframe width="560" height="315" src="//www.youtube.com/embed/c9SUAcNlVQ4" frameborder="0" allowfullscreen></iframe>
+					</figure>
+					<div class="Categoria espiritualidad">
+						<h4><a href="#"><strong>Espiritualidad</strong></a></h4>
+					</div>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Pampa mía</a></h3>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox">
+				<div class="InnerBox">
+					<figure class="Video">
+						<iframe width="560" height="315" src="//www.youtube.com/embed/c9SUAcNlVQ4" frameborder="0" allowfullscreen></iframe>
+					</figure>
+					<div class="Categoria cultura">
+						<h4><a href="#"><strong>Cultura</strong></a></h4>
+					</div>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Amar el fin del mundo</a></h3>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox">
+				<div class="InnerBox">
+					<figure class="Video">
+						<iframe width="560" height="315" src="//www.youtube.com/embed/c9SUAcNlVQ4" frameborder="0" allowfullscreen></iframe>
+					</figure>
+					<div class="Categoria solidaridad">
+						<h4><a href="#"><strong>Solidaridad</strong></a></h4>
+					</div>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">De Vietnam con amor</a></h3>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox">
+				<div class="InnerBox">
+					<figure class="Video">
+						<iframe width="560" height="315" src="//www.youtube.com/embed/c9SUAcNlVQ4" frameborder="0" allowfullscreen></iframe>
+					</figure>
+					<div class="Categoria espiritualidad">
+						<h4><a href="#"><strong>Espiritualidad</strong></a></h4>
+					</div>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Un mundo mejor es posible</a></h3>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="InfoBox">
+				<div class="InnerBox">
+					<figure class="Video">
+						<iframe width="560" height="315" src="//www.youtube.com/embed/c9SUAcNlVQ4" frameborder="0" allowfullscreen></iframe>
+					</figure>
+					<div class="Categoria cultura">
+						<h4><a href="#"><strong>Cultura</strong></a></h4>
+					</div>
+					<div class="Data">
+						<div class="InnerData">
+							<h3><a href="#">Una estrella en el bajo Flores</a></h3>
+						</div>
+						<div class="SocialToolBar">
+							<p><a href="#" class="Comentarios"><i class="fa fa-comments"></i>14</a> <span class="IconSep"></span> <span class="Share"><span class="Label">Compartí </span> <span class="LabelLayer"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-google-plus"></i></a></span></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+		<div class="LoadBar">
+			<a href="#"><strong>+ Videos</strong></a>
+		</div>
+	</div>
+</main>
+<!-- BANNER -->
+<?php include("inc_bottom-banner.php"); ?>
+<?php include("inc_footer.php"); ?>
+<?php include("inc_foot.php"); ?>
+</body>
+</html>
