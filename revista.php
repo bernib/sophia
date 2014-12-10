5c9b571a5e681e699561ee7e82af135209df8d77
Alejandro Jarandilla <alejandroj@publiland.com.ar>
2014-12-10 10:55:07 -0300

    Pagina 404

--- revista.php
+++ revista.php
@@ -12,56 +12,12 @@
 <?php include("inc_header.php"); ?>
 <!-- MAIN NAV -->
 <?php include("inc_nav.php"); ?>
-<!-- CATEGORIA -->
-<section class="Row CatHead la-revista">
-	<h2>La Revista</h2>
-</section>
 <!-- CONTENT -->
 <main class="Row">
 	<div class="Wrapper">
-		<article class="Row PediLaRevista">
-			<h3>Suscribite a Sophia <span class="Line"></span> <span>4 ejemplares al año</span></h3>
-			<div class="row">
-				<div class="Modalidad">
-					<figure>
-						<figcaption>
-							<span>Desde diciembre 2014</span><br><strong>Nueva modalidad</strong>
-						</figcaption>
-						<img src="images/revista/revista-clip.jpg" width="370" height="280" alt=""/>
-					</figure>
-					<h4><strong>4 ejemplares al año</strong></h4>
-					<ul>
-						<li><i class="fa fa-sun-o"></i><strong>DICIEMBRE</strong></li>
-						<li><i class="fa fa-sun-o"></i><strong>MARZO</strong></li>
-						<li><i class="fa fa-sun-o"></i><strong>JUNIO</strong></li>
-						<li><i class="fa fa-sun-o"></i><strong>SEPTIEMBRE</strong></li>
-					</ul>
-				</div>
-				<div class="Intro">
-					<p class="Texto">SOPHIA es una publicación de interés general que cuestiona los estereotipos de género y propone otros modelos de mujer. SOPHIA revaloriza la sabiduría, la espiritualidad, los vínculos y los valores, a través de historias inspiradoras. SOPHIA se dirige a quienes buscan integrar su cuerpo con su alma, sin dejar de lado las ideas. Más que una revista o una plataforma digital, es una causa que necesita de todos, mujeres y varones, para crecer. Aquí encontrarás entrevistas a fondo, notas de actualidad y nuevas tendencias. ¡Sumate!</p>
-					<div class="Tapa">
-						<figure><a href="#"><img src="images/revista/revista-tapa.jpg" width="150" height="198" alt=""/></a></figure>
-						<div class="Data">
-							<h5><strong>Última Edición</strong></h5>
-							<h4><strong>#154</strong><br>
-								<span>Octubre 2014</span></h4>
-							<a href="#" class="Lnk">Sumario</a>
-							<a href="#" class="Lnk">Ediciones anteriores</a>
-						</div>
-					</div>
-					<div class="Row">
-						<div class="Precio">
-							<h4><strong>$55</strong> ARS*</h4>
-							<p><strong>cada ejemplar + envío</strong></p>
-						</div>
-						<a href="#" class="Btn"><strong>¡COMPRAR AHORA!</strong></a>
-						<div class="MedioPago">
-							<p><em>* varía de acuerdo al precio de tapa.</em></p>
-							<p><strong>Compra seguro con tu tarjeta de crédito a través de:</strong> <a href="#"><img src="images/revista/mercado-pago.png" width="94" height="23" alt=""/></a></p>
-						</div>
-					</div>
-				</div>
-			</div>
+		<article class="Row Msj404">
+			<h2>La página a la cual intentás acceder, no está disponible.</h2>
+			<h3>Puedes regresar a la página principal del sitio <a href="index.php">desde aqui</a></h3>
 		</article>
 	</div>
 	<div class="Row Pedila">
