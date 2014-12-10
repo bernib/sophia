8a7bea16b366d1bbc62dcbad376ec6019613c97a
Alejandro Jarandilla <alejandroj@publiland.com.ar>
2014-12-10 11:42:12 -0300

    404 update

--- 404.php
+++ 404.php
@@ -19,32 +19,6 @@
 			<h2>La página a la cual intentás acceder, no está disponible.</h2>
 			<h3>Puedes regresar a la página principal del sitio <a href="index.php">desde aqui</a></h3>
 		</article>
-	</div>
-	<div class="Row Pedila">
-		<div class="Wrapper">
-			<ul>
-				<li>
-					<h4><i class="fa fa-caret-right"></i><strong>Recibila en tu domicilio</strong></h4>
-					<p>Comprando ahora, te enviamos la revista a la <br>
-					dirección que indiques dentro de Argentina. Si vivís<br> en el exterior, <a href="#">consultá las condiciones para tu país.</a></p>
-				</li>
-				<li>
-					<h4><i class="fa fa-caret-right"></i><strong>Cancelá en cualquier momento</strong></h4>
-					<p>Dale de baja a tu suscripción <a href="#">aquí</a>. No te <br>
-					cobramos cargos adicionales. </p>
-				</li>
-				<li>
-				<h4><i class="fa fa-caret-right"></i><strong>¿PREGUNTAS?</strong></h4>
-					<p>Escribinos a <a href="mailto:info@vivisophia.com">info@vivisophia.com</a><br>
-					o llamá al <strong>(54 11) 4833.5353.</strong><br>
-					Te contactaremos dentro de las 48 hs.</p>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<div class="Row Pedila2">
-		<p class="Wrapper">* Conseguila también kioscos de revistas de Buenos Aires y en <a href="#">puntos de distribución</a></p>
-	</div>
 </main>
 <?php include("inc_footer.php"); ?>
 <?php include("inc_foot.php"); ?>
